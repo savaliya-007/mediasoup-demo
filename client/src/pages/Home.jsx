5ade286580d09e483062a76ad68410d6e45b732d
umesh <umeshsavaliya777@gmail.com>
2026-02-12 15:33:22 +0530

    Some files added

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -9,8 +9,8 @@ export default function Home({ setView, setUser, setRoom }) {
   const canProceedJoin = name.trim().length > 0 && roomInput.trim().length > 0;
 
   return (
-    <div className="min-h-screen bg-gradient-to-br from-gray-900 via-gray-800 to-black flex items-center justify-center text-white">
-      <div className="bg-gray-900/80 backdrop-blur-md border border-gray-700 rounded-2xl shadow-2xl p-8 w-[350px] space-y-6">
+    <div className="min-h-screen bg-linear-to-br from-gray-900 via-gray-800 to-black flex items-center justify-center text-white">
+      <div className="bg-gray-900/80 backdrop-blur-md border border-gray-700 rounded-2xl shadow-2xl p-8 w-[87.5] space-y-6">
         <h1 className="text-2xl font-bold text-center tracking-wide">
           Live Broadcast
         </h1>
@@ -19,14 +19,14 @@ export default function Home({ setView, setUser, setRoom }) {
         {!mode && (
           <div className="space-y-3">
             <button
-              className="w-full bg-gradient-to-r from-blue-600 to-indigo-600 py-2 rounded-lg font-semibold"
+              className="w-full bg-linear-to-r from-blue-600 to-indigo-600 py-2 rounded-lg font-semibold"
               onClick={() => setMode("create")}
             >
               Create Broadcast
             </button>
 
             <button
-              className="w-full bg-gradient-to-r from-purple-600 to-pink-600 py-2 rounded-lg font-semibold"
+              className="w-full bg-linear-to-r from-purple-600 to-pink-600 py-2 rounded-lg font-semibold"
               onClick={() => setMode("join")}
             >
               Join Broadcast
@@ -64,8 +64,8 @@ export default function Home({ setView, setUser, setRoom }) {
               disabled={mode === "create" ? !canProceedCreate : !canProceedJoin}
               className={`w-full py-2 rounded-lg font-semibold transition disabled:opacity-40 ${
                 mode === "create"
-                  ? "bg-gradient-to-r from-blue-600 to-indigo-600"
-                  : "bg-gradient-to-r from-purple-600 to-pink-600"
+                  ? "bg-linear-to-r from-blue-600 to-indigo-600"
+                  : "bg-linear-to-r from-purple-600 to-pink-600"
               }`}
               onClick={() => {
                 setUser(name);
