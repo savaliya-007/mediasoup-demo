b564e14749ee5ef1deac126e3913a128852b0161
umesh <umeshsavaliya777@gmail.com>
2026-02-13 17:24:52 +0530

    Added .env,dev,start script

--- client/src/services/socket.js
+++ client/src/services/socket.js
@@ -1,5 +1,5 @@
 import { io } from "socket.io-client";
-export const socket = io("http://localhost:8000");
+export const socket = io(import.meta.env.VITE_SOCKET_URL);
 
 export const initSocketEvents = (store) => {
   socket.on("room:users", (users) => store.setUsers(users));
